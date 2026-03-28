f6ac3de82cef36a1ec0df927e58b988b32690190
IsacNewtoniano <isacnewtoneano@gmail.com>
2026-03-28 00:29:50 -0300

    Add Simple-Types

---
+++ Types.h
@@ -0,0 +1,64 @@
+#pragma once
+/*
+  dt:-----------------------------
+  Default: All API: Types & Macros
+  dt:-------------------------
+*/
+
+/*
+  te:---------------------
+  vTypes: Integers & Floats & Pointers
+  te:-----------------
+*/
+
+
+//type: Signed 8-Bits
+typedef signed char i8;
+//type: Unsigned 8-Bits
+typedef unsigned char u8;
+
+//type: Signed 8-Bits Pointer
+typedef signed char *i8pt;
+//type: Unsigned 8-Bits Pointer
+typedef unsigned char *u8pt;
+
+
+//type: Signed 16-Bits
+typedef signed short i16;
+//type: Unsigned 16-Bits
+typedef unsigned short u16;
+
+//type: Signed 16-Bits Pointer
+typedef signed short *i16pt;
+//type: Unsigned 16-Bits Pointer
+typedef unsigned short *u16pt;
+
+
+//type: Signed 32-Bits
+typedef signed int i32;
+//type: Unsigned 32-Bits
+typedef unsigned int u32;
+//type: Float 32-Bits
+typedef float f32;
+
+//type: Signed 32-Bits Pointer
+typedef signed int *i32pt;
+//type: Unsigned 32-Bits Pointer
+typedef unsigned int *u32pt;
+//type: Float 32-Bits Pointer
+typedef float *f32pt;
+
+
+//type: Signed 64-Bits
+typedef signed long long int i64;
+//type: Unsigned 64-Bits
+typedef unsigned long long int u64;
+//type: Float 64-Bits
+typedef float f64;
+
+//type: Signed 64-Bits Pointer
+typedef signed long long int *i64pt;
+//type: Unsigned 64-Bits Pointer
+typedef unsigned long long int *u64pt;
+//type: Float 64-Bits Pointer
+typedef double  *f64pt;
