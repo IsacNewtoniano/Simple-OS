7bec382a16bf619e815436c863a8634a81357d7d
IsacNewtoniano <isacnewtoneano@gmail.com>
2026-03-28 01:52:24 -0300

    Reorganization & New Types

--- Types.h
+++ Types.h
@@ -7,58 +7,103 @@
 
 /*
   te:---------------------
-  vTypes: Integers & Floats & Pointers
+  vTypes: Signed Integers
   te:-----------------
 */
 
-
 //type: Signed 8-Bits
 typedef signed char i8;
-//type: Unsigned 8-Bits
-typedef unsigned char u8;
-
-//type: Signed 8-Bits Pointer
-typedef signed char *i8pt;
-//type: Unsigned 8-Bits Pointer
-typedef unsigned char *u8pt;
-
 
 //type: Signed 16-Bits
 typedef signed short i16;
-//type: Unsigned 16-Bits
-typedef unsigned short u16;
-
-//type: Signed 16-Bits Pointer
-typedef signed short *i16pt;
-//type: Unsigned 16-Bits Pointer
-typedef unsigned short *u16pt;
-
 
 //type: Signed 32-Bits
 typedef signed int i32;
-//type: Unsigned 32-Bits
-typedef unsigned int u32;
-//type: Float 32-Bits
-typedef float f32;
-
-//type: Signed 32-Bits Pointer
-typedef signed int *i32pt;
-//type: Unsigned 32-Bits Pointer
-typedef unsigned int *u32pt;
-//type: Float 32-Bits Pointer
-typedef float *f32pt;
-
 
 //type: Signed 64-Bits
 typedef signed long long int i64;
-//type: Unsigned 64-Bits
-typedef unsigned long long int u64;
-//type: Float 64-Bits
-typedef float f64;
+
+
+//type: Signed 8-Bits Pointer
+typedef signed char i8_pointer;
+
+//type: Signed 16-Bits Pointer
+typedef signed short i16_pointer;
+
+//type: Signed 32-Bits Pointer
+typedef signed int i32_pointer;
 
 //type: Signed 64-Bits Pointer
-typedef signed long long int *i64pt;
+typedef signed long long int i64_pointer;
+
+
+
+/*
+  te:----------------------
+  vTypes: Unsigned Integers
+  te:------------------
+*/
+
+//type: Unsigned 8-Bits
+typedef unsigned char u8;
+
+//type: Unsigned 16-Bits
+typedef unsigned short u16;
+
+//type: Unsigned 32-Bits
+typedef unsigned int u32;
+
+//type: Unsigned 64-Bits
+typedef unsigned long long int u64;
+
+
+//type: Unsigned 8-Bits Pointer
+typedef unsigned char u8_pointer;
+
+//type: Unsigned 16-Bits Pointer
+typedef unsigned short u16_pointer;
+
+//type: Unsigned 32-Bits Pointer
+typedef unsigned int u32_pointer;
+
 //type: Unsigned 64-Bits Pointer
-typedef unsigned long long int *u64pt;
-//type: Float 64-Bits Pointer
-typedef double  *f64pt;
+typedef unsigned long long int u64_pointer;
+
+
+
+/*
+  te:-----------
+  vTypes: Floats
+  te:-------
+*/
+
+//type: Unsigned 32-Bits
+typedef float f32;
+
+//type: Unsigned 64-Bits
+typedef double f64;
+
+
+//type: Unsigned 32-Bits Pointer
+typedef float f32_pointer;
+
+//type: Unsigned 64-Bits Pointer
+typedef double f64_pointer;
+
+/*
+  ea:----------------
+  vTypes: Other Types
+  Extra: Bool
+  ea:------------
+*/
+
+typedef void non_return, *generic_pointer;
+
+#ifndef __bool_true_false_are_defined
+
+typedef enum : u8 {
+  false = 0,
+  true = 1
+} bool;
+
+#endif
